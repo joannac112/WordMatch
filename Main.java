331f9b6ff3e8160425e96f1f83b2317cd459f421
joannac112 <joannac112@nycstudents.net>
2025-03-21 13:30:08 +0000

    save1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args{
+        WordMatch w = new WordMatch("mississippi")
+        system.out.println(w.scoreGuess("i"));
+        system.out.println(w.scoreGuess("iss"));
+        system.out.println(w.scoreGuess("issipp"));
+        system.out.println(w.scoreGuess("mississippi"));
+    }
+}
